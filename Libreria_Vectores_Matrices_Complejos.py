fe4c776164d5f6469cd28ffc816eefeb925b5758
JAPV-X2612 <143054558+JAPV-X2612@users.noreply.github.com>
2023-09-06 23:48:29 -0500

    Actualización de Funciones
    Se añaden a la librería las funciones 9 a 17, faltando únicamente el producto tensor pendiente por explicación

--- Libreria_Vectores_Matrices_Complejos.py
+++ Libreria_Vectores_Matrices_Complejos.py
@@ -2,7 +2,7 @@
 # Carrera / Semestre: Ingeniería de Sistemas / 4to Semestre
 # Asignatura: Ciencias Naturales y Tecnología (CNYT) 
 # Nombre: Jesús Alfonso Pinzón Vega
-# Fecha: 2023/08/31
+# Fecha: 2023/09/06
 
 # Librería de Operaciones con Vectores y Matrices Complejos
 
@@ -12,9 +12,12 @@ from numpy import *
 def cplx_vect_sum(v1,v2):
     """Calcula y devuelve la suma de dos vectores complejos
     (1D array, 1D array) -> 1D array"""
-    v1,v2 = array(v1),array(v2),
-    vr = v1 + v2
-    return vr
+    try:
+        v1,v2 = array(v1),array(v2)
+        vr = v1 + v2
+        return vr
+    except:
+        return "Los vectores NO son compatibles"
 
 
 def cplx_vect_add_inver(v):
@@ -27,7 +30,7 @@ def cplx_vect_add_inver(v):
 
 def cplx_vect_scal_mult(c,v):
     """Calcula y devuelve la multiplicación de un escalar por un vector complejo
-    (complex or int ot float, 1D array) -> 1D array"""
+    (int or float or complex, 1D array) -> 1D array"""
     v = array(v)
     vr = c*v
     return vr
@@ -36,9 +39,12 @@ def cplx_vect_scal_mult(c,v):
 def cplx_mtx_sum(m1,m2):
     """Calcula y devuelve la suma de matrices complejas
     (2D array, 2D array) -> 2D array"""
-    m1,m2 = array(m1),array(m2)
-    mr = m1 + m2
-    return mr
+    try:
+        m1,m2 = array(m1),array(m2)
+        mr = m1 + m2
+        return mr
+    except:
+        return "Las matrices NO son compatibles"
 
 
 def cplx_mtx_add_inver(m):
@@ -51,7 +57,7 @@ def cplx_mtx_add_inver(m):
 
 def cplx_mtx_scal_mult(c,m):
     """Calcula y devuelve la multiplicación de un escalar por una matriz compleja
-    (complex or int ot float, 1D array) -> 1D array"""
+    (int ot float or complex, 1D array) -> 1D array"""
     m = array(m)
     mr = c*m
     return mr
@@ -68,7 +74,7 @@ def cplx_vct_mtx_conj(vm):
     """Calcula y devuelve el conjugado de un vector o matriz complejos
     (1D array or 2D array) -> 1D array or 2D array"""
     vm = array(vm)
-    vmc = vm.conjugate()
+    vmc = conjugate(vm)
     return vmc
 
 
@@ -76,5 +82,102 @@ def cplx_vct_mtx_adj(vm):
     """Calcula y devuelve la adjunta (daga) de un vector o matriz complejos
     (1D array or 2D array) -> 1D array or 2D array"""
     vm = array(vm)
-    vma = transpose(vm.conjugate())
+    vma = transpose(conjugate(vm))
     return vma
+
+
+def cplx_mtx_prod(m1,m2):
+    """Calcula y devuelve el producto de dos matrices complejas
+    (2D array, 2D array) -> 2D array"""
+    try: 
+        m1,m2 = array(m1),array(m2)
+        mr = dot(m1,m2)
+        return mr
+    except:
+        return "Las matrices NO son compatibles"
+        
+        
+def cplx_vct_mtx_act(m,v):
+    """Calcula y devuelve la acción de una matriz compleja sobre un vector complejo
+    (2D array, 1D array) -> 1D array"""
+    try:
+        m,v = array(m),array(v)
+        vma = dot(m,v)
+        return vma
+    except:
+        return "La matriz y el vector NO son compatibles"
+
+
+def cplx_vct_mtx_inter_prod(vm1,vm2):
+    """Calcula y devuelve el producto interno de dos vectores o matrices complejos
+    (1D array, 1D array or 2D array, 2D array) -> 1D array or 2D array or int or float"""
+    try:
+        vm1,vm2 = array(vm1),array(vm2)
+        d1 = vm1.ndim
+        d2 = vm2.ndim
+        if d1 == 1 == d2:
+            vip = dot(cplx_vct_mtx_adj(vm1),vm2)
+            return vip
+        elif vm1.shape[0] == vm2.shape[0]:
+            mip = trace(dot(cplx_vct_mtx_adj(vm1),vm2))
+            return mip
+        else:
+            return "Los vectores o matrices NO son compatibles"
+    except:
+        return "Los vectores o matrices NO son compatibles"
+        
+        
+def cplx_vct_mtx_norm(vm):
+    """Calcula y devuelve la norma de un vector o matriz complejos
+    (1D array or 2D array) -> int or float"""
+    vm = array(vm)
+    vmn = linalg.norm(vm)
+    return(vmn)
+    
+
+def cplx_vct_mtx_dist(vm1,vm2):
+    """Calcula y devuelve la distancia entre dos vectores o matrices complejos
+    (1D array, 1D array or 2D array, 2D array) -> int or float"""
+    vm1,vm2 = array(vm1),array(vm2)
+    dist = linalg.norm(vm1-vm2)
+    return dist
+
+ 
+def cplx_mtx_val_vect(m):
+    """Calcula y devuelve los valores y vectores propios de una matriz compleja
+    (2D array) -> 1D array, 2D array"""
+    m = array(m)
+    val,vect = linalg.eig(m)
+    return val,vect
+
+
+def cplx_unt_mtx(m):
+    """Indica si una matriz compleja es unitaria
+    (2D array) -> True or False"""
+    m = array(m)
+    a,b = m.shape
+    if a == b:
+        unit = [list(x) for x in dot(m,cplx_vct_mtx_adj(m))]
+        ident = [list(x) for x in identity(a)]
+        if  unit == ident :
+            return True
+        else:
+            return False
+    else:
+        return "La matriz No es cuadrada"
+
+
+def cplx_herm_mtx(m):
+    """Indica si una matriz compleja es hermitiana
+    (2D array) -> True or False"""
+    m = array(m)
+    a,b = m.shape
+    if a == b:
+        hm = [list(x) for x in m]
+        hmt = [list(x) for x in cplx_vct_mtx_adj(m)]
+        if  hm == hmt:
+            return True
+        else:
+            return False
+    else:
+        return "La matriz No es cuadrada"
---
+++ Pruebas Libreria_Vectores_Matrices_Complejos.py
@@ -0,0 +1,179 @@
+# ESCUELA COLOMBIANA DE INGENIERÍA JULIO GARAVITO 
+# Carrera / Semestre: Ingeniería de Sistemas / 4to Semestre
+# Asignatura: Ciencias Naturales y Tecnología (CNYT) 
+# Nombre: Jesús Alfonso Pinzón Vega
+# Fecha: 2023/09/06
+
+# Pruebas de la Librería de Operaciones con Vectores y Números Complejos
+
+from Libreria_Vectores_Matrices_Complejos import *
+import unittest as ut
+
+class Test_Libreria_Nombre(ut.TestCase):
+    
+    def test_cplx_vect_sum(self):
+        """Prueba del cálculo de la suma de dos vectores complejos
+        None -> OK or FAILED (failures=#)"""
+        vr = list(cplx_vect_sum([1+2j, 5-2j], [4+4j, -1+7j]))
+        self.assertEqual(vr, [5+6j, 4+5j])
+        vr = list(cplx_vect_sum([1.5j, 3, -1-1.2j], [7, 4.7+9.2j, 3j]))
+        self.assertEqual(vr, [7+1.5j, 7.7+9.2j, -1+1.8j])
+    
+    
+    def test_cplx_vect_add_inver(self):
+        """Prueba del cálculo del inverso aditivo de un vector complejo
+        None -> OK or FAILED (failures=#)"""
+        vr = list(cplx_vect_add_inver([2-5.7j, 7+4j]))
+        self.assertEqual(vr, [-2+5.7j, -7-4j])
+        vr = list(cplx_vect_add_inver([-1-5j, 1j, -1j]))
+        self.assertEqual(vr, [1+5j, -1j, +1j])
+        
+        
+    def test_cplx_vect_scal_mult(self):
+        """Prueba del cálculo de la suma de dos vectores complejos
+        None -> OK or FAILED (failures=#)"""
+        vr = list(cplx_vect_scal_mult(9, [1+2j, 5-2j]))
+        self.assertEqual(vr, [9+18j, 45-18j])
+        vr = list(cplx_vect_scal_mult(2-3j, [7, 4+9j, 2.5-3j]))
+        self.assertEqual(vr, [14-21j, 35+6j, -4-13.5j])
+    
+      
+    def test_cplx_mtx_sum(self):
+        """Prueba del cálculo de la suma de dos matrices complejas
+        None -> OK or FAILED (failures=#)"""
+        mr = [list(m) for m in cplx_mtx_sum([[1+3j, -5-7j], [4-1j, 8+4j]], [[1+1j, 2-6j], [9+9j, -10+20j]])]
+        self.assertEqual(mr, [[2+4j, -3-13j], [13+8j, -2+24j]])
+        mr = [list(m) for m in cplx_mtx_sum([[-33+2j, 6-74j], [14-8j, 18-4j]], [[-4+1j, 11-1j], [21+7j, -1+2j]])]
+        self.assertEqual(mr, [[-37+3j, 17-75j], [35-1j, 17-2j]])
+        
+    
+    def test_cplx_mtx_add_inver(self):
+        """Prueba del cálculo de la inversa aditiva de una matriz compleja
+        None -> OK or FAILED (failures=#)"""
+        mr = [list(m) for m in cplx_mtx_add_inver([[1+3j, -5-7j], [4-1j, 8+4j]])]
+        self.assertEqual(mr, [[-1-3j, 5+7j], [-4+1j, -8-4j]])
+        mr = [list(m) for m in cplx_mtx_add_inver([[-4+1j, 11-1j], [21+7j, -1+2j]])]
+        self.assertEqual(mr, [[4-1j, -11+1j], [-21-7j, 1-2j]])
+        
+    
+    def test_cplx_mtx_scal_mult(self):
+        """Prueba del cálculo de la multiplicación de un escalar por una matriz compleja
+        None -> OK or FAILED (failures=#)"""
+        vmt = [list(m) for m in cplx_mtx_scal_mult(2+3j,[[1+3j, -5-7j], [4-1j, 8+4j]])]
+        self.assertEqual(vmt, [[-7+9j, 11-29j], [11+10j, 4+32j]])
+        vmt = [list(m) for m in cplx_mtx_scal_mult(5,[[-4+1j, 11-1j], [21+7j, -1+2j]])]
+        self.assertEqual(vmt, [[-20+5j, 55-5j], [105+35j, -5+10j]])
+     
+        
+    def test_cplx_vct_mtx_trans(self):
+        """Prueba del cálculo de la transpuesta de un vector o matriz complejos
+        None -> OK or FAILED (failures=#)"""
+        mr = list(cplx_vct_mtx_trans([1+3j, -5-7j, 4-1j, 8+4j]))
+        self.assertEqual(mr, [1+3j, -5-7j, 4-1j, 8+4j])
+        mr = [list(m) for m in cplx_vct_mtx_trans([[-4.6+3j, 1.1-1j, 2j], [21+7j, 4, -1+2j], [-15.2+9j, -1-4.4j, -100+2.2j]])]
+        self.assertEqual(mr, [[-4.6+3j, 21+7j, -15.2+9j], [1.1-1j, 4+0j, -1-4.4j], [2j, -1+2j, -100+2.2j]])
+    
+    
+    def test_cplx_vct_mtx_conj(self):
+        """Prueba del cálculo del conjugado de un vector o matriz complejos
+        None -> OK or FAILED (failures=#)"""
+        vmc = list(cplx_vct_mtx_conj([1+3j, -5-7j, 4-1j, 8+4j]))
+        self.assertEqual(vmc, [1-3j, -5+7j, 4+1j, 8-4j])
+        vmc = [list(m) for m in cplx_vct_mtx_conj([[-4.6+3j, 1.1-1j, 2j], [21+7j, 4, -1+2j], [-15.2+9j, -1-4.4j, -100+2.2j]])]
+        self.assertEqual(vmc, [[-4.6-3j, 1.1+1j, -2j], [21-7j, 4, -1-2j], [-15.2-9j, -1+4.4j, -100-2.2j]])
+
+
+    def test_cplx_vct_mtx_adj(self):
+        """Prueba del cálculo de la adjunta (daga) de un vector o matriz complejos
+        None -> OK or FAILED (failures=#)"""
+        vma = list(cplx_vct_mtx_adj([1+3j, -5-7j, 4-1j, 8+4j]))
+        self.assertEqual(vma, [1-3j, -5+7j, 4+1j, 8-4j])
+        vma = [list(m) for m in cplx_vct_mtx_adj([[-4.6+3j, 1.1-1j], [21+7j,-1+2j]])]
+        self.assertEqual(vma, [[-4.6-3j, 21-7j], [1.1+1j, -1-2j]])
+    
+    
+    def test_cplx_mtx_prod(self):
+        """Prueba del cálculo del producto de dos matrices complejas
+        None -> OK or FAILED (failures=#)"""
+        mr = [list(m) for m in cplx_mtx_prod([[1+3j, -5+1j], [4-1j, 8+4j]], [[1-1j, 2-6j], [3+9j, -4+2j]])]
+        self.assertEqual(mr, [[-20-40j, 38-14j], [-9+79j, -38-26j]])
+        mr = [list(m) for m in cplx_mtx_prod([[-1+2j, 6-7j], [1-8j, 5]], [[1j, 1-1j], [1+7j, 0]])]
+        self.assertEqual(mr, [[53+34j, 1+3j], [13+36j, -7-9j]])
+        
+    
+    def test_cplx_vct_mtx_act(self):
+        """Prueba del cálculo de la acción de una matriz compleja sobre un vector complejo
+        None -> OK or FAILED (failures=#)"""
+        vma = list(cplx_vct_mtx_act([1+3j, -5+1j], [[1-1j, 2-6j], [3+9j, -4+2j]]))
+        self.assertEqual(vma, [-20-40j, 38-14j])
+        vma = list(cplx_vct_mtx_act([-2, 1j], [[1j, 1-1j], [1+7j, 0]]))
+        self.assertEqual(vma, [-7-1j, -2+2j])
+        
+    
+    def test_cplx_vct_mtx_inter_prod(self):
+        """Prueba del cálculo del producto interno de dos vectores o matrices complejos
+        None -> OK or FAILED (failures=#)"""
+        vip = cplx_vct_mtx_inter_prod([1+3j, -5+1j], [1-1j, 2-6j])
+        self.assertEqual(vip, -18+24j)
+        mip = cplx_vct_mtx_inter_prod([[-1+2j, 1], [1-3j, 5]], [[1j, 1-1j], [1+7j, 0]])
+        self.assertEqual(mip, -17+8j)
+        
+    
+    def test_cplx_vct_mtx_norm(self):
+        """Prueba del cálculo de la norma de un vector o matriz complejos
+        None -> OK or FAILED (failures=#)"""
+        vmn = cplx_vct_mtx_norm([1+7j, 4-1j, 2-6j])
+        self.assertAlmostEqual(vmn, 10.34408043)
+        vmn = cplx_vct_mtx_norm([[-1+2j, -3j], [1-3j, 5]])
+        self.assertAlmostEqual(vmn, 7)
+        
+    
+    def test_cplx_vct_mtx_dist(self):
+        """Prueba del cálculo de la distancia entre dos vectores o matrices complejos
+        None -> OK or FAILED (failures=#)"""
+        dist = cplx_vct_mtx_dist([-1+2j, 6-7j, 1-8j], [4+1j, -12, 1-1j])
+        self.assertAlmostEqual(dist, 21.166010488)
+        dist = cplx_vct_mtx_dist([[-1+2j, 1], [1-3j, 0]], [[1j, 1-1j], [1+7j, 0]])
+        self.assertAlmostEqual(dist, 10.148891565)
+    
+    
+    def test_cplx_mtx_val_vect(self):
+        """Prueba del cálculo de los valores y vectores propios de una matriz compleja
+        None -> OK or FAILED (failures=#)"""
+        val,vect = cplx_mtx_val_vect([[-1j, 1], [0, 1j]])
+        val,vect = list(val), [list(x) for x in vect]
+        self.assertEqual(val, [-1j, 1j])
+        self.assertAlmostEqual(vect[0][0], 1)
+        self.assertAlmostEqual(vect[0][1], -0.447213595j)
+        self.assertAlmostEqual(vect[1][0], 0)
+        self.assertAlmostEqual(vect[1][1], 0.894427190)
+        
+        val,vect = cplx_mtx_val_vect([[2-3j, 0], [3, -5+2j]])
+        val,vect = list(val), [list(x) for x in vect]
+        self.assertEqual(val, [-5+2j, 2-3j])
+        self.assertAlmostEqual(vect[0][0], 0)
+        self.assertAlmostEqual(vect[0][1], 0.94422787)
+        self.assertAlmostEqual(vect[1][0], 1)
+        self.assertAlmostEqual(vect[1][1], 0.26795656+0.19139754j)
+        
+    
+    def test_cplx_unt_mtx(self):
+        """Prueba de la validación de una matriz compleja unitaria
+        None -> OK or FAILED (failures=#)"""
+        ans = cplx_unt_mtx([[1j, 0, 0], [0, 1j, 0], [0, 0, 1j]])
+        self.assertEqual(ans, True)
+        ans = cplx_unt_mtx([[-1+2j, 1], [1-3j, 0]])
+        self.assertEqual(ans, False)
+    
+    
+    def test_cplx_herm_mtx(self):
+        """Prueba de la validación de una matriz compleja hermitiana
+        None -> OK or FAILED (failures=#)"""
+        ans = cplx_herm_mtx([[3, 3-8j, -1-1j], [3+8j, 1, -5-6j], [-1+1j, -5+6j, -2]])
+        self.assertEqual(ans, True)
+        ans = cplx_herm_mtx([[-1+2j, 1], [1-3j, 0]])
+        self.assertEqual(ans, False)
+
+    
+if __name__ == '__main__':
+    ut.main()
